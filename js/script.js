2a55e83cd828f9ba2bd1a907a6eaa096f1f5acea
MehmetPolat20 <mehmet.polat20@hotmail.com>
2024-05-24 20:54:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+searchForm = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+let loginForm = document.querySelector(".login-form-container");
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
+window.onload = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
+var swiper = new Swiper(".books-slider", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
